98bbbcc0cbc40dd989e655d3af52f2bbe38c0142
Horacio Mota <horaciomota93@gmail.com>
2024-02-13 17:45:08 +0000

    first commit

---
+++ src/Application.kt
@@ -0,0 +1,16 @@
+fun main() {
+    print("Hello World sem o LN")
+    println("Hello World")
+
+
+    var test2  = 3
+    val test = 123
+
+    println(test2)
+    test2 = 4
+
+    println(test2)
+
+    // PrintLN é linha!
+}
+
